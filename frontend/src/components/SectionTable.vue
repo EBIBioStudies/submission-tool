d11a1b905384a1f3a189763881ac71b639f57d40
Awais Athar <awais@ebi.ac.uk>
2023-07-14 21:48:01 +0100

    Add draggable table rows

--- frontend/src/components/SectionTable.vue
+++ frontend/src/components/SectionTable.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import draggable from 'vuedraggable';
 
 const props = defineProps(['rows']);
 const headerMap = new Map();
@@ -11,6 +12,7 @@ props.rows.forEach((row) =>
 );
 const keys = [...headerMap.keys()];
 const headers = ref(keys);
+const theseRows = ref(props.rows);
 const getCell = (row, col) => {
   return row.attributes.find(
     (att) => att.name.toLowerCase() === col.toLowerCase(),
@@ -21,27 +23,33 @@ const getCell = (row, col) => {
   <div class="ps-3">
     <table class="table table-responsive">
       <thead>
-        <tr>
-          <th v-for="(header, i) in headers" :key="i">
-            <input
-              class="form-control form-control-sm header"
-              type="text"
-              v-model="headers[i]"
-            />
-          </th>
-        </tr>
+        <draggable
+          v-model="headers"
+          tag="tr"
+          :item-key="(key) => key"
+          @start="dragging = true"
+          @end="dragging = false"
+        >
+          <template #item="{ element: header }">
+            <th>
+              {{ header }}
+            </th>
+          </template>
+        </draggable>
       </thead>
-      <tbody>
-        <tr v-for="(row, i) in rows" :key="i">
-          <td v-for="(col, j) in headers" :key="j">
-            <input
-              class="form-control form-control-sm"
-              type="text"
-              v-model="getCell(row, col).value"
-            />
-          </td>
-        </tr>
-      </tbody>
+      <draggable v-model="theseRows" tag="tbody" item-key="name">
+        <template #item="{ element: row }">
+          <tr>
+            <td v-for="(col, j) in headers" :key="j">
+              <input
+                class="form-control form-control-sm"
+                type="text"
+                v-model="getCell(row, col).value"
+              />
+            </td>
+          </tr>
+        </template>
+      </draggable>
     </table>
   </div>
 </template>
