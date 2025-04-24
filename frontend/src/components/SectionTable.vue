dbb56b57c79c85e0da4dc53f989eb0dd259eb25a
ehsan <ehsan@ebi.ac.uk>
2025-04-25 00:17:14 +0100

    all subsection sections must be flat and expanded [#189080994]

--- frontend/src/components/SectionTable.vue
+++ frontend/src/components/SectionTable.vue
@@ -159,7 +159,10 @@ const updateColumnName = ( value, index) => {
   emits('columnUpdated', { old: oldValue, new: newValue, index: index });
 };
 
-const isCollapsed = ref((props?.depth ?? 0) >= 2 || props.startCollapsed);
+const isCollapsed = ref(
+  props.startCollapsed === undefined
+    ? (props?.depth ?? 0) >= 2
+    : props.startCollapsed);
 const toggle = () => (isCollapsed.value = !isCollapsed.value);
 
 const attributeRefs = ref([]);
