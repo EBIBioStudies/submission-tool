d4e8e589116e3594504da6b6e02c594176c2a09d
Eliot Ragueneau <eragueneau@ebi.ac.uk>
2026-03-17 13:53:14 +0000

    :memo: Add doc on let component

--- frontend/src/components/Let.vue
+++ frontend/src/components/Let.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts" generic="T">
+
+/**
+ * Allow to define a variable within a template to avoid code repetition and recalculating things
+ *
+ * Usage:
+ * <Let :value="...heavyCalculation" v-slot="{ value: nameOfTheVariable }"
+ */
 defineProps<{
+  /**
+   * The value to put inside the variable
+   */
   value: T
-}>()
+}>();
 
 defineSlots<{
   default(props: { value: T }): any
-}>()
+}>();
 </script>
 
 <template>
