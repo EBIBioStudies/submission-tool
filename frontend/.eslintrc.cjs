a7ca827534cc8c78cd9f167936e16cd9f36764be
Eliot Ragueneau <eragueneau@ebi.ac.uk>
2026-05-29 13:23:05 +0100

    :rotating_light: Adjust linter

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,11 +4,17 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'standard',
-    'plugin:vue/vue3-essential',
-    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.vue', 'src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
     {
       env: {
         node: true,
@@ -19,11 +25,17 @@ module.exports = {
       },
     },
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
+    extraFileExtensions: ['.vue'],
   },
-  plugins: ['vue'],
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
   rules: {
     'vue/multi-word-component-names': 0,
   },
